6401aa6114d276320ef89a0f92d6555dbdd9f0b7
jwPark <semanticist0@gmail.com>
2024-06-23 13:37:10 +0900

    ✨ docx

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
   import shared from "$lib/shared";
+  import mammoth from "mammoth";
+  import { writable } from "svelte/store";
 
+  const htmlContent = writable<string>("");
+
+  const test = async () => {
+    const file = await fetch("/example.docx");
+    const arrayBuffer = await file.arrayBuffer();
+
+    const result = await mammoth.convertToHtml({ arrayBuffer });
+    htmlContent.set(result.value); 
+  };
+
+  test();
 </script>
 
 <div>
   <title>{shared.immutable.CONSTS.APP_NAME}</title>
-  hello!
+    {@html $htmlContent}
+    <td>asdasd</td>
 </div>
 
 <style>
+  :global(td) {
+    border: 1px solid #000;
+  }
 </style>
